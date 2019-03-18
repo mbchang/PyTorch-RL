a33eb2f2801d595cab1fae79dc005018cce6e1f6
Michael Chang <mbchang2017@gmail.com>
2019-03-18 11:31:44 -0700

    notes for experiment

--- run_ant.py
+++ run_ant.py
@@ -7,6 +7,12 @@ import os
 Adam vs SGD
 Actor Learning Rate
 Critic Learning Rate
+
+Conclusion: 
+eplen of 200 is better for debugging. 
+Before with eplen of 10000, since the episode was so long, it took an extremely long time for the ant to learn because of the credit assignment problem. It saturated max reward around 6000, but most of this is just from survival.
+By using eplen of 200, it saturates the survival reward at 200, after which in order to get larger reward it has to start moving forward.
+So what is likely a better strategy is to train for 200 but test on 10000.
 """
 optimizer = ['sgd', 'adam']
 plr = [5e-5]
@@ -19,6 +25,10 @@ outputdir = 'runs/ant_test_optimizer_lr_eplen'
 gpu = True
 num_gpus = 2
 i = 0
+
+if gpu:
+    os.system('export OMP_NUM_THREADS=1')
+
 for o, p, c, e, l in itertools.product(optimizer, plr, clr, envs, eplen):
     prefix = 'CUDA_VISIBLE_DEVICES={} '.format(i) if gpu else ''
     command = 'python examples/ppo_gym.py --opt {} --plr {} --clr {} --env-name {} --maxeplen {}'.format(o, p, c, e, l)
