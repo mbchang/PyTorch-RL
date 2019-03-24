fixed broadcasting issue in log_prob. removed VIB for now

core/rl_algs.py
@@ -25,7 +25,9 @@ class PPO():
         masks = torch.from_numpy(np.stack(batch.mask)).to(self.dtype).to(self.device)
         with torch.no_grad():
             values = self.agent.valuefn(states)
-            fixed_log_probs, _, _ = self.agent.policy.get_log_prob(states, actions)
+            # fixed_log_probs, _, _ = self.agent.policy.get_log_prob(states, actions)
+            fixed_log_probs, _ = self.agent.policy.get_log_prob(states, actions)
+            # fixed_log_probs = self.agent.policy.get_log_prob(states, actions)
 
         """get advantage estimation from the trajectories"""
         advantages, returns = estimate_advantages(rewards, masks, values, self.args.gamma, self.args.tau, self.device)
@@ -61,18 +63,24 @@ class PPO():
             self.agent.value_optimizer.step()
 
         """update policy"""
-        log_probs, kl, entropy = self.agent.policy.get_log_prob(states, actions)
+        # log_probs, kl, entropy = self.agent.policy.get_log_prob(states, actions)
+        log_probs, entropy = self.agent.policy.get_log_prob(states, actions)
+        # log_probs = self.agent.policy.get_log_prob(states, actions)
         ratio = torch.exp(log_probs - fixed_log_probs)
         surr1 = ratio * advantages
         surr2 = torch.clamp(ratio, 1.0 - self.args.clip_epsilon, 1.0 + self.args.clip_epsilon) * advantages
         policy_surr = -torch.min(surr1, surr2).mean()  # mean over batch
         entropy_penalty = self.args.entropy_coeff * entropy.mean()  # mean over batch
-        ib_penalty = self.args.klp * kl.mean()  # mean over batch
+        # ib_penalty = self.args.klp * kl.mean()  # mean over batch
+        ib_penalty = 0
         policy_surr += ib_penalty - entropy_penalty  # maximize entropy
         self.agent.policy_optimizer.zero_grad()
         policy_surr.backward()
         torch.nn.utils.clip_grad_norm_(self.agent.policy.parameters(), 40)
         self.agent.policy_optimizer.step()
 
+        """log"""
+        num_clipped = (surr1-surr2).nonzero().size(0)
+        ratio_clipped = num_clipped / states.size(0)
 
 

models/functions.py
@@ -88,12 +88,12 @@ class GaussianParams(nn.Module):
             self.mu.weight.data.mul_(0.1)
             nn.init.constant_(self.mu.bias, 0.0)
 
-            self.logstd.weight.data.mul_(0.1)
+            self.logstd.weight.data.mul_(0.1)  # Can take out
             nn.init.constant_(self.logstd.bias, np.log(0.1))
 
     def forward(self, x):
         mu = self.mu(x)
         logstd = self.logstd(x)
-        return mu, logstd#torch.exp(logstd)
+        return mu, logstd
 
 

models/primitives.py
@@ -48,16 +48,17 @@ class GaussianVIBPolicy(nn.Module):
     def get_log_prob(self, state, action):
         mu, std, kl = self.forward(state)
         dist = MultivariateNormal(loc=mu, scale_tril=torch.diag_embed(std))
-        log_prob = dist.log_prob(action)
-        entropy = dist.entropy()
-        return log_prob, kl, entropy
+        log_prob = dist.log_prob(action)  # keep dims=True
+        entropy = dist.entropy()  # keep
+        bsize = state.size(0)
+        return log_prob.view(bsize, 1), kl.view(bsize, 1), entropy.view(bsize, 1)
 
-class PrimitivePolicy(GaussianVIBPolicy):
+class PrimitiveVIBPolicy(GaussianVIBPolicy):
     """
 
     """
     def __init__(self, encoder, bottleneck_dim, decoder_dims, device, fixed_var=False):
-        super(PrimitivePolicy, self).__init__()
+        super(PrimitiveVIBPolicy, self).__init__()
         self.outdim = decoder_dims[-1]
         self.encoder = encoder
         self.ib = InformationBottleneck(encoder.dims[-1], bottleneck_dim, device=device)
@@ -70,6 +71,50 @@ class PrimitivePolicy(GaussianVIBPolicy):
         h = F.relu(self.decoder(z))
         mu, logstd = self.parameter_producer(h)
         return mu, torch.exp(logstd), kl
+
+class GaussianPolicy(nn.Module):
+    def __init__(self):
+        super(GaussianPolicy, self).__init__()
+        self.is_disc_action = False
+
+    def forward(self, x):
+        raise NotImplementedError
+
+    def select_action(self, state, deterministic=False):
+        mu, std = self.forward(state)
+        if deterministic:
+            return mu
+        else:
+            dist = MultivariateNormal(loc=mu, scale_tril=torch.diag_embed(std))
+            action = dist.sample()
+            return action
+
+    def get_log_prob(self, state, action):
+        mu, std = self.forward(state)
+        dist = MultivariateNormal(loc=mu, scale_tril=torch.diag_embed(std))
+        log_prob = dist.log_prob(action)  # keep dims=True
+        entropy = dist.entropy()  # keep
+        bsize = state.size(0)
+        return log_prob.view(bsize, 1), entropy.view(bsize, 1)
+
+class PrimitivePolicy(GaussianPolicy):
+    """
+
+    """
+    def __init__(self, encoder, bottleneck_dim, decoder_dims, device, fixed_var=False):
+        super(PrimitivePolicy, self).__init__()
+        self.outdim = decoder_dims[-1]
+        self.encoder = encoder
+        self.ib = nn.Linear(encoder.dims[-1], bottleneck_dim)
+        self.decoder = nn.Linear(bottleneck_dim, decoder_dims[0])
+        self.parameter_producer = GaussianParams(decoder_dims[0], decoder_dims[1], custom_init=True, fixed_var=fixed_var)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        z = self.ib(x)
+        h = F.relu(self.decoder(z))
+        mu, logstd = self.parameter_producer(h)
+        return mu, torch.exp(logstd)
 
 class CompositePolicy(GaussianVIBPolicy):
     def __init__(self, weight_network, primitives):
@@ -104,7 +149,7 @@ def debug2():
     state_dim = 60
     action_dim = 12
     encoder = Feedforward([state_dim, 512, 256], out_act=F.relu)
-    policy = PrimitivePolicy(encoder=encoder, ib_dims=[256, 128], hdim=256, outdim=action_dim)
+    policy = PrimitiveVIBPolicy(encoder=encoder, ib_dims=[256, 128], hdim=256, outdim=action_dim)
     print(policy)
 
 def debug():